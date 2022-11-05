0f4873bdd3791bc10b651d10532402ba1614f19a
Vishal-508 <vishalsharma331441@gmail.com>
2022-11-05 09:31:14 +0530

    an

--- Construct Week Unit-3 Orbitz Website/scripts/listPage.js
+++ Construct Week Unit-3 Orbitz Website/scripts/listPage.js
@@ -10,7 +10,7 @@ const options = {
 const destinationID = JSON.parse(localStorage.getItem("destinationID")); 
 // console.log(destinationID)
 async function showData(){
-	let url = `https://hotels-com-provider.p.rapidapi.com/v1/hotels/search?checkin_date=2023-10-20&checkout_date=2023-10-27&sort_order=STAR_RATING_HIGHEST_FIRST&destination_id=${destinationID}&adults_number=1&locale=en_US&currency=USD&children_ages=4%2C0%2C15&price_min=10&star_rating_ids=3%2C4%2C5&accommodation_ids=20%2C8%2C15%2C5%2C1&price_max=500&page_number=1&theme_ids=14%2C27%2C25&amenity_ids=527%2C2063&guest_rating_min=4` ; 
+	let url = `https://hotels-com-provider.p.rapidapi.com/v1/hotels/search?checkin_date=2023-01-20&checkout_date=2023-01-27&sort_order=STAR_RATING_HIGHEST_FIRST&destination_id=${destinationID}&adults_number=1&locale=en_US&currency=USD&children_ages=4%2C0%2C15&price_min=10&star_rating_ids=3%2C4%2C5&accommodation_ids=20%2C8%2C15%2C5%2C1&price_max=500&page_number=1&theme_ids=14%2C27%2C25&amenity_ids=527%2C2063&guest_rating_min=4` ; 
 
 
 

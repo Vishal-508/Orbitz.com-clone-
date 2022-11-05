e884667df346148f8b632df4a45d1676dafd3975
Vishal-508 <vishalsharma331441@gmail.com>
2022-11-05 09:13:27 +0530

    api

--- Construct Week Unit-3 Orbitz Website/scripts/hotelPage.js
+++ Construct Week Unit-3 Orbitz Website/scripts/hotelPage.js
@@ -6,7 +6,7 @@ let hotelID = hotelDetails.id ;
 const option = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '2988766804msh3709a2157f0e0d3p1f1e2djsn983587b3c261',
+		'X-RapidAPI-Key': '715564a4f0msh58a53e432b88722p16f800jsn040c97c70ac0',
 		'X-RapidAPI-Host': 'hotels-com-provider.p.rapidapi.com'
 	}
 };
